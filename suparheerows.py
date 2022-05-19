e20d3b72109a0fb8ba87908802e043096cdba7d7
mosserryan <mosser.ryan.a@users.noreply.github.com>
2022-05-19 17:57:06 +0000

    Learning about conditional if

---
+++ suparheerows.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+heroes = {
+"Real Name":"Wade Winston Wilson",
+"First Appearance": "New Mutants (Vol. 1) #98 (February, 1991)",
+"Creators": ["Fabian Nicieza", "Rob Liefeld"],
+"Team Affiliations": ["Thunderbolts", "X-Force", "Agency X", "Deadpool Corps.", "Great Lakes Initiative", "Weapon X", "Landau", "Luckman", "X-Men"],
+"Skills and abilities": ["Master martial artist", "swordsman and marksman", "Master strategist and improviser", "has several voices in his head the represent parts of himself that give him advice", "easily defies expectation in most instances"]
+}
+print(heroes)
